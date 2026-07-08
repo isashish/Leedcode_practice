4d64e04ff8441bfc77b3c8a1375ae7c89625c509
Ashish Suryavanshi <168853855+isashish@users.noreply.github.com>
2026-07-08 20:15:17 +0530

    Time: 2 ms (14.28%), Space: 42.9 MB (68.17%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,44 @@
 class Solution {
+    private final int PRIME = 101;
+
+    private long calculateHash(String str){
+        long hash = 0;
+        for(int i  = 0; i < str.length(); i++){
+            hash = (long)(hash + str.charAt(i) * Math.pow(PRIME, i));
+        }
+        return hash;
+    }
+
+    private long updateHash(long prevHash, char oldChar, char newChar, int patternLength){
+        long newHash = (prevHash - oldChar)/PRIME;
+        newHash = (long)(newHash + newChar * Math.pow(PRIME, patternLength-1));
+        return newHash;
+    }
+
+    private int search(String text, String pattern){
+        int patternLength = pattern.length();
+        long patternHash = calculateHash(pattern);
+        long textHash = calculateHash(text.substring(0, patternLength));
+
+        for(int i = 0; i <= text.length() - patternLength; i++){
+            if(patternHash == textHash){
+                if(text.substring(i, i+patternLength).equals(pattern)){
+                    return i;
+                }
+            }
+            if(i < text.length() - patternLength){
+                textHash = updateHash(textHash, text.charAt(i), text.charAt(i+patternLength), patternLength);
+            }
+        }
+        return -1;
+    }
+
+
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if(haystack.length() >= needle.length()){
+            return this.search(haystack, needle);
+        }
+        return -1;
+        
     }
 }
